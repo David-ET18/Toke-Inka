99d82171a1f707c91f78e7cb55a25a4752811f7c
DAVID <18davidescatetorres@gmail.com>
2025-11-28 11:05:49 -0500

    test corregio

--- src/test/java/com/example/toke/TokeApplicationTests.java
+++ src/test/java/com/example/toke/TokeApplicationTests.java
@@ -1,17 +1,16 @@
 package com.example.toke;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase; // <--- IMPORTANTE
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles; // <-- ¡AÑADE ESTE IMPORT!
 
 @SpringBootTest
-@ActiveProfiles("test") // <-- ¡AÑADE ESTA ANOTACIÓN!
+// ESTA LÍNEA ES LA QUE ARREGLA EL ERROR:
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) 
 class TokeApplicationTests {
 
-    // Ya no necesitamos Testcontainers, así que eliminamos todo ese código.
+	@Test
+	void contextLoads() {
+	}
 
-    @Test
-    void contextLoads() {
-        // Esta prueba ahora se ejecutará forzando el perfil 'test'
-    }
 }
